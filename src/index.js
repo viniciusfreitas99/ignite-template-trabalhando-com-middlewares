00be3465d8a3a392ef0b80a1e51a0397b9c3ab3b
vinicius <viniciusgomesdefreitas999@gmail.com>
2021-06-10 17:45:18 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "user not found" });
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const pro = user.pro;
+
+  if ((pro === false && user.todos.lenght <10) || pro === true) {
+    return next();
+  } 
+  if (pro === false && user.todos.lenght === 10 ) {
+    return response.status(403).json();
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (user) {
+    return response.status(404);
+  }
+  if (validate(id)) {
+    return response.status(400);
+  }
+  if (todo) {
+    return response.status(404);
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "user not found" }); 
+  }
+  request.user = user;
+  return nesxt();
 }
 
 app.post('/users', (request, response) => {
